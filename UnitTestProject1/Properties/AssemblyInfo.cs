8d3f4f082865a6cb9ce022bcabaf1eaf5c9b4885
Nikolai Nikolov <nikkolai4o@gmail.com>
2022-12-01 12:55:25 +0200

    unit test

---
+++ UnitTestProject1/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestProject1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestProject1")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("66cb0645-8a11-4ea5-94da-959afce7687d")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var math = new GitUnitTestDemo.Math(5,int.MaxValue);
+            var math2 = new GitUnitTestDemo.Math(int.MaxValue, 0);
+            Assert.AreNotEqual(7, math.Add());
+            Assert.AreNotEqual(3, math.Subtract());
+            Assert.AreNotEqual(10, math.Multiply());
+            Assert.AreNotEqual(2.5f, math.Divide());
+            Assert.AreEqual(Single.PositiveInfinity, math2.Divide());
+            //OR
+            Assert.IsTrue(Single.IsPositiveInfinity(math2.Divide()));
+            Assert.AreNotEqual(14, math2.SumDigits());
+            Assert.AreEqual("101",math.Binary());
+        }
+    }
+}
